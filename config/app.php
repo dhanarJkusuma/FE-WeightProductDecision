e978f4469515315935b2c690be5eeac828f51a36
Dhanar J Kusuma <Dhanar J Kusuma>
2017-02-12 00:35:49 +0700

    fix big dependency

--- config/app.php
+++ config/app.php
@@ -175,8 +175,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        Vsmoraes\Pdf\PdfServiceProvider::class
+        App\Providers\RouteServiceProvider::class
     ],
 
     /*
@@ -225,7 +224,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => 'Vsmoraes\Pdf\PdfFacade',
     ],
 
 ];
