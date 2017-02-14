25c47680fe7fccb6d1a147616a4e331d61cd7631
Dhanar J Kusuma <Dhanar J Kusuma>
2017-02-15 00:26:02 +0700

    fix menu

--- resources/views/component/menu_user.blade.php
+++ resources/views/component/menu_user.blade.php
@@ -16,7 +16,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li id="li-dashboard"><a href="{{ url('home') }}">Dashboard</a></li>
-                <li id="li-list"><a href="{{ url('home/calculate')  }}">Daftar Nilai</a></li>
+                <li id="li-list"><a href="{{ url('home/calculate')  }}">Lihat Perhitungan</a></li>
                 <li id="li-print"><a class="btn-print" href="{{ url('print')  }}">Cetak Laporan</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
